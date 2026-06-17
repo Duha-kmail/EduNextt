80114e401e41e542a14574b9b71f82c3d2bf502a
duha kmail <d.alhajsaleh@student.aaup.edu>
2026-06-17 16:53:54 +0300

    .

--- start-ai.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0ai_chatbot"
-call start_ai.cmd %*
--- start-ai.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [switch]$Foreground
-)
-
-$ErrorActionPreference = "Stop"
-$script = Join-Path $PSScriptRoot "ai_chatbot\start_ai.ps1"
-& $script @PSBoundParameters
